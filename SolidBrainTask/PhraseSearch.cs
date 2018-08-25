1ffebfc5e1412fb1366a38757b836b766ccd982a
MichalMi <42695408+MichalMi@users.noreply.github.com>
2018-08-25 14:18:42 +0200

    Init repo

---
+++ SolidBrainTask/PhraseSearch.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SolidBrainTask
+{
+    public class PhraseSearch
+    {
+        static IWebDriver driver = new ChromeDriver();
+        static IWebElement searchBar;
+
+        public string Phrase { get; set; }
+        
+        public bool Search()
+        {
+            string url = "https://new.abb.com";
+            driver.Navigate().GoToUrl(url);
+            
+            Thread.Sleep(1000);
+
+            ClickSearchIcon();
+
+            searchBar = driver.FindElement(By.Id("search"));
+
+            searchBar.SendKeys(Phrase);
+            searchBar.SendKeys(Keys.Enter);
+
+            Thread.Sleep(3000);
+
+            var items = driver.FindElements(By.ClassName("OneABBSearchList-item"));
+            bool result = false;
+
+            for(var i = 0; i < 3; i++)
+            {
+                result = FindPhraseInItem(items[i]);
+
+                if(!result)
+                {
+                    break;
+                }
+            }
+            
+            driver.Quit();
+            return result;
+        }
+
+        private void ClickSearchIcon()
+        {
+            driver.FindElement(By.ClassName("abb-icon__search")).Click();
+        }
+
+        private bool FindPhraseInItem(IWebElement item)
+        {
+            var headingElement = item.FindElement(By.ClassName("OneABBSearchList-item-heading"));
+
+            if (headingElement.Text.ToLower().Contains(Phrase.ToLower()))
+            {
+                return true;
+            }
+
+            var paragraphElements = item.FindElements(By.ClassName("OneABBSearchList-item-paragraph"));
+
+            foreach(var paragraphElement in paragraphElements)
+            {
+                if (paragraphElement.Text.ToLower().Contains(Phrase.ToLower()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ SolidBrainTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace SolidBrainTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phraseSearch = new PhraseSearch()
+            {
+                Phrase = "Yumi"
+            };
+
+            if(phraseSearch.Search())
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            Thread.Sleep(3000);
+        }
+    }
+}
