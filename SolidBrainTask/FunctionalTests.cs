e360de28e6aa7bb261ede9e21941206be07fc21f
Mateusz Zieliński <mateusz.zielinski@pl.abb.com>
2018-08-27 12:04:59 +0200

    NUnit framework used to do the testing

---
+++ SolidBrainTask/FunctionalTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SolidBrainTask
+{
+    [TestFixture]
+    internal class FunctionalTests
+    {
+        [Test]
+        public void SearchTest()
+        {
+            Assert.IsTrue(new PhraseSearch().Search("Yumi"), "Search is not working properly.");
+        }
+    }
+}
